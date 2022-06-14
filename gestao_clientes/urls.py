5480b2fbfea2c43d7f56863f63709fad54ea86bc
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-06-14 17:42:28 +0100

    tudo list, fixme

--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -19,13 +19,15 @@ from clientes import urls as clientes_urls
 from home import urls as home_urls
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views, login
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
+
 
 
 urlpatterns = [
     path('', include(home_urls)),
     path('clientes/', include(clientes_urls)),
-    path('login/', auth_views.login, name='login'),
+    # path('login/', auth_views.login, name='login'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- home/views.py
+++ home/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
 
+# TODO: refatorar para usar threads assim que possivel
 def home(request):
     return render(request, 'home.html')
 
-
+# FIXME: corrigir bugs
 def my_logout(request):
     logout(request)
     return redirect('home')
